6eec375922d50ad4eba1abf0bc8ce88d3a69ee64
Zoheb0707 <zhs142013@yahoo.in>
2019-06-20 20:56:04 -0700

    made spacing uniform

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 /**
-  Author: Zoheb0707
-  Created: 06/19/2019
+    Author: Zoheb0707
+    Created: 06/19/2019
 */
 
 /* setting up database */
@@ -9,27 +9,27 @@ USE maps;
 
 -- creating users table
 CREATE TABLE users(
-  user_id varchar(20) NOT NULL,
-  password varchar(20) NOT NULL,
-  first_name varchar(20) NOT NULL,
-  last_name varchar(20) NOT NULL,
-  PRIMARY KEY(user_id)
+    user_id varchar(20) NOT NULL,
+    password varchar(20) NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    PRIMARY KEY(user_id)
 );
 
 /* creating table restaurants */
 CREATE TABLE restaurants(
-  restaurant_id varchar(20) NOT NULL,
-  name varchar(40) NOT NULL,
-  type varchar(20),
-  location varchar(100) NOT NULL,
-  PRIMARY KEY(restaurant_id)
+    restaurant_id varchar(20) NOT NULL,
+    name varchar(40) NOT NULL,
+    type varchar(20),
+    location varchar(100) NOT NULL,
+    PRIMARY KEY(restaurant_id)
 );
 
 /* creating table visits */
 CREATE TABLE visits(
-  restaurant_id varchar(20) NOT NULL,
-  user_id varchar(20) NOT NULL,
-  times int NOT NULL,
-  avg_rating float NOT NULL,
-  PRIMARY KEY(restaurant_id, user_id)
+    restaurant_id varchar(20) NOT NULL,
+    user_id varchar(20) NOT NULL,
+    times int NOT NULL,
+    avg_rating float NOT NULL,
+    PRIMARY KEY(restaurant_id, user_id)
 );
