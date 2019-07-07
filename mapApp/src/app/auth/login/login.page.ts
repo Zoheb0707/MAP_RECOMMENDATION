06a41771b4ab1f67db439a383ff022ebc45febbc
Andrei <andy.karavanov@gmail.com>
2019-07-06 22:28:58 -0700

    Primitive Login

--- mapApp/src/app/auth/login/login.page.ts
+++ mapApp/src/app/auth/login/login.page.ts
@@ -1,23 +1,107 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+@Injectable()
+export class LoginPage {
 
-  constructor(private  router: Router) { }
-
-  ngOnInit() {
+  constructor(private  router: Router, private http: HttpClient, private alertCtrl: AlertController, private storage: Storage) {
+    this.storage.get('id').then((val) => {
+      if (val != null) {
+        this.router.navigateByUrl('/app/tabs/search');
+      }
+    });
   }
 
   navTabs(form) {
-    if (form.value.email === 'root' && form.value.password === 'root') {
-      this.router.navigateByUrl('/app/tabs/search');
+    let mode = '';
+    if (form.value.email != null && form.value.password != null) {
+      mode = form.value.email;
     } else {
-      alert('Wrong Credentials!');
+      mode = 'all';
     }
+
+    this.getData(mode)
+    .then(data => {
+      if (data[0].password === form.value.password) {
+        this.storage.set('id', data[0].user_id);
+        this.storage.set('first_name', data[0].first_name);
+        this.storage.set('last_name', data[0].last_name);
+        this.router.navigateByUrl('/app/tabs/search');
+      } else {
+        alert("Wrong Password!");
+      }
+    });
+  }
+
+  getData(mode: string) {
+      const url = 'http://localhost:80/get_users.php?mode=' + mode;
+
+      // this.http.get(url).subscribe((data: Response) => {
+      //   // console.log(data);
+      //   toReturn = data;
+      // }, error => {
+      //   console.log(error);
+      // });
+      // // console.log(toReturn);
+      // return toReturn;
+
+      return new Promise(resolve => {
+        this.http.get(url)
+          .subscribe((data: any) => {
+            resolve(data);
+          }, error => {
+            // resolve(error);
+          });
+      });
+
+      // return new Promise(resolve => {
+      //   this.http.get(url)
+      //     .subscribe((data: any) => {
+      //       resolve(data.map(res => {
+      //         return new User(
+      //           res.last_name,
+      //           res.first_name,
+      //           res.user_id,
+      //           res.password
+      //         );
+      //       }));
+      //     }, error => {
+      //       resolve(error);
+      //     });
+      // });
+
+//       return this.http.get(url).pipe(map(res => {
+// // tslint:disable-next-line: no-use-before-declare
+//           return new User(
+//             res.first_name,
+//             res.last_name,
+//             res.user_id,
+//             res.password
+//             );
+//         }));
   }
 }
+
+// export class User {
+//   firstName = '';
+//   lastName = '';
+//   id = '';
+//   password = '';
+//     constructor(firstName, lastName, id, password) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.id = id;
+//         this.password = password;
+//     }
+// }
--- mapApp/src/app/tab3/tab3.page.ts
+++ mapApp/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab3',
@@ -8,9 +9,18 @@ import { Router } from '@angular/router';
 })
 export class Tab3Page {
 
-  constructor(private  router: Router) {}
+  name: string;
+
+  constructor(private  router: Router, private storage: Storage) {
+    this.storage.get('first_name').then((val: string) => {
+      this.name = val;
+    });
+  }
 
   async onChangeExit() {
+    this.storage.set('id', null);
+    this.storage.set('first_name', null);
+    this.storage.set('last_name', null);
     this.router.navigateByUrl('login');
   }
 }
