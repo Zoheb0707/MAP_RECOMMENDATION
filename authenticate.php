8e2b8cb19ba7cf07fb4c24ffdca59a9be65fdf0a
KAR-PC <andy.karavanov@gmail.com>
2019-07-10 23:44:10 -0700

    Auth returns user object

--- authenticate.php
+++ authenticate.php
@@ -1,4 +1,9 @@
 <?php
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT");
+    header("Access-Control-Allow-Headers': Origin, Content-Type, application/json");
+
     /**
         Author: Zoheb0707
         Created: 07/04/2019
@@ -10,20 +15,20 @@
     include "database_object.php";
 
 
-    if (isset($_GET["user"]) && isset($_GET["passwd"])) {
-        $user = strtolower($_GET["user"]);
-        $passwd = $_GET["passwd"];
+    if (isset($_POST["user_id"]) && isset($_POST["password"])) {
+        $user = strtolower($_POST["user_id"]);
+        $passwd = $_POST["password"];
         if(contains_user($user)) {
             check_password($user, $passwd);
         }
         else {
-            header("HTTP/1.1 400 Invalid Request");
+            header("HTTP/1.1 404 Not Found");
             header("Content-type: text/plain");
-            print("{$_GET["user"]} is not a user");
+            print("User not found!");
         }
     }
     else {
-        header("HTTP/1.1 400 Invalid Request");
+        header("HTTP/1.1 400 Bad Request");
         header("Content-type: text/plain");
         print("Enter user and passwd parameters");
     }
@@ -32,12 +37,17 @@
         $user_info = get_user($user);
         $true_passwd = $user_info["password"];
         if ($true_passwd === $passwd) {
-            header("Content-type: text/plain");
-            print("True");
+	    $expiresIn = 24 * 60 * 60;
+            $response->user = $user_info;
+	    $response->expires_in = $expiresIn;		
+	    header("HTTP/1.1 200 OK");
+            header('Content-type: application/json');
+            echo json_encode($response); 
         }
         else {
+            header("HTTP/1.1 401 Unauthorized");
             header("Content-type: text/plain");
-            print("False");
+            print("Password not valid!");
         }
 
     }
