ff668af088fef639913a42aaffc2fc6a481c0939
KAR-PC <andy.karavanov@gmail.com>
2019-07-15 06:50:55 -0700

    Results are rendered as ion-card

--- mapApp/src/app/searches/searches.page.ts
+++ mapApp/src/app/searches/searches.page.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs';
+import { SuggestionsService } from '../services/suggestions.service';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-searches',
@@ -8,12 +14,19 @@ import { Router } from '@angular/router';
 })
 export class SearchesPage implements OnInit {
 
-  constructor(private  router: Router) { }
+  results: Observable<any>;
+
+  constructor(private  router: Router, private http: HttpClient, private suggestions: SuggestionsService, private storage: Storage, 
+              private navCtrl: NavController) { }
 
   ngOnInit() {
+      this.storage.get('ID').then((val: string) => {
+        this.results = this.suggestions.getSuggestions(val);
+      });
+
   }
 
   back() {
-    this.router.navigateByUrl('/app/tabs/search');
+    this.navCtrl.navigateBack('app/tabs/search');
   }
 }
