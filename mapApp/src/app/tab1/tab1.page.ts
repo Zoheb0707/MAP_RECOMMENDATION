2afd07c983d7a49926c1844ffcb3bda0ae897d5f
KAR-PC <andy.karavanov@gmail.com>
2019-07-21 00:06:12 -0700

    Refreshing animation timing
    Refreshing of the past visits finishes once it gets the response from the API.

--- mapApp/src/app/tab1/tab1.page.ts
+++ mapApp/src/app/tab1/tab1.page.ts
@@ -4,8 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { Observable } from 'rxjs';
 import { VisitsService } from '../services/visits.service';
-import { Restaurant } from './restaurant';
-import { restoreView } from '@angular/core/src/render3';
 
 
 @Component({
@@ -17,9 +15,7 @@ export class Tab1Page implements OnInit {
 
   results: Observable<any>;
 
-  // public listOfPlaces: Array<Restaurant>;
-
-  constructor(private  router: Router, private http: HttpClient, private visitsService: VisitsService, private storage: Storage) {}
+  constructor(private router: Router, private http: HttpClient, private visitsService: VisitsService, private storage: Storage) { }
 
   ngOnInit() {
     this.updateList();
@@ -31,29 +27,16 @@ export class Tab1Page implements OnInit {
 
 
   doRefresh(event) {
-    setTimeout(() => {
-      this.updateList();
-      event.target.complete();
-    }, 500);
+    this.updateList(event);
   }
 
-  updateList() {
-    // this.storage.get('ID').then((val: string) => {
-    //   this.results = this.visitsService.searchData(val);
-    //   this.visitsService.searchData(val).subscribe(res => {
-    //     res.forEach((val) => {
-    //       const currentRest = new Restaurant(val);
-    //       if (!this.listOfPlaces.includes(currentRest)) {
-    //         this.listOfPlaces.push(currentRest);
-    //         console.log('Updated!');
-    //         console.log(this.listOfPlaces);
-    //       }
-    //     });
-    //   });
-    // });
-
+  async updateList(event?) {
     this.storage.get('ID').then((val: string) => {
       this.results = this.visitsService.searchData(val);
+      if (event !== undefined) {
+        event.target.complete();
+      }
     });
   }
+
 }
