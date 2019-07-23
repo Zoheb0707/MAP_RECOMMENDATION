ca900b6e9c6581a01b04dace3b831850342534e8
KAR-PC <andy.karavanov@gmail.com>
2019-07-23 14:58:12 -0700

    Page design
    Simple profile page design

--- mapApp/src/app/tab3/tab3.page.ts
+++ mapApp/src/app/tab3/tab3.page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
 
 import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/user';
 
 @Component({
   selector: 'app-tab3',
@@ -11,21 +12,43 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['tab3.page.scss']
 })
 
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
   name: string;
 
-  constructor(private  authService: AuthService,private  router: Router, private storage: Storage, private navCtrl: NavController) {
+  user: User = {user_id: '',
+                first_name: '',
+                last_name: '',
+                password: '',
+  };
+
+  mySlideOptions = {
+    pager:true
+  };
+
+  constructor(private  authService: AuthService, private  router: Router, private storage: Storage, private navCtrl: NavController) {
   }
 
-  ionViewWillEnter() {
-    this.storage.get('FIRST_NAME').then((val: string) => {
-      this.name = val;
+  ngOnInit() {
+    this.getUser();
+  }
+
+  async getUser() {
+    await this.storage.get('FIRST_NAME').then((val: string) => {
+      this.user.first_name = val;
+    });
+
+    await this.storage.get('LAST_NAME').then((val: string) => {
+      this.user.last_name = val;
+    });
+
+    await this.storage.get('user_id').then((val: string) => {
+      this.user.user_id = val;
     });
   }
 
-  ionViewDidLeave() {
-    this.name = '';
+  editProfile() {
+    alert('Let\'s change the profile!');
   }
 
   async onChangeExit() {
